1c4f524cf6051f58519ccd2b1e4bd496ef8b33de
Sergio Nieto <s.nieto.t@gmail.com>
2025-02-10 23:50:57 -0800

    Randomize success images

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './css/Image.css';
 
-function Image({
-}: {
-  }) {
+function Image() {
+  const imageNumber = Math.floor(Math.random() * 4);
+  const imgArr = ['duos-hug.gif', 'duos-kiss-1.gif', 'duos-kiss-2.gif', 'duos-kiss-3.gif'];
 
   return (
     <div className='tmp-rule'>
       <div className='polaroid-frame'>
-        <img className='picture' src='duos-hug.gif' alt='Milk and Mocha' />
+        <img className='picture' src={imgArr[imageNumber]} alt='Milk and Mocha' />
       </div>
     </div>
   );
